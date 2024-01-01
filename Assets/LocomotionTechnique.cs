a764062a90d689cd8de24760260483b020eea2b2
EduardE02 <eduard@rantsevich.net>
2024-01-01 20:49:57 +0100

    old locomotion technique, now this is in the ship script

---
+++ Assets/LocomotionTechnique.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionTechnique : MonoBehaviour
+{
+
+    public OVRInput.Controller leftController;
+    public OVRInput.Controller rightController;
+    public Transform leftControllerTransform;  // Assign the left controller Transform
+    public Transform rightControllerTransform; // Assign the right controller Transform
+    private Vector3 startPos;
+    float maxThrust = 10.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, leftController);
+        float rightTriggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, rightController);
+
+        rb.AddForce((-leftTriggerValue * leftControllerTransform.forward.normalized - rightTriggerValue * rightControllerTransform.forward.normalized) * maxThrust * Time.deltaTime, ForceMode.Impulse);
+    }
+}
